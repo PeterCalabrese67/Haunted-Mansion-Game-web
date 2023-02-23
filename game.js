806c03a523e57a4f62b3dc13b7e6f2ff5b6ee663
Peter Calabrese <petercal@optonline.net>
2023-02-23 15:12:25 -0500

    go,look,take work

--- game.js
+++ game.js
@@ -11,7 +11,8 @@ const rooms = {
       exits: {
         south: "foyer",
         north: "library"
-      }
+      },
+      items: []
     },
     library: {
       description: "You are in the library. The room is musty and smells of old books. There are rows of bookshelves lining the walls, and a large desk in the center of the room. On the desk, you see a thick, leather-bound book with strange symbols on the cover.",
@@ -51,23 +52,17 @@ const objects = {
   let currentRoom = "foyer";
   
   // Show the current room
-function showRoom() {
-    print(rooms[currentRoom].description);
-    if (rooms[currentRoom].items.length > 0) {
-      print("You see the following items in the room:");
-      rooms[currentRoom].items.forEach((item) => {
-        print(objects[item].name);
-      });
-    }
-  }
+
   function describeRoom(room) {
     //console.log(rooms[room].name);
+    if (room){
     print(rooms[room].description);
     if (rooms[room].items.length > 0) {
       print("You see the following items in the room:");
       rooms[room].items.forEach((item) => {
         print(objects[item].name);
       });
+    
     }
 
     const currentRoom = rooms[room];
@@ -76,7 +71,8 @@ function showRoom() {
     //const exits = [...rooms[room].exits.keys()].join(", ");
     print(`Exits: ${exits}`);
     print ("<br><br> What do you want to do now?");
-  }
+   } // if room
+  } //describeRoom
 
   function processCommand(command) {
     const parts = command.split(" ");
@@ -86,55 +82,43 @@ function showRoom() {
     if (!action) {
       printResponse("I don't understand what you want me to do.");
     } else if (action === "look") {
-      describeRoom(currentRoom);
+        if (isRoom (target)) {
+            describeRoom(currentRoom);
+        }
+        else {
+            look (target);
+        }
     } else if (action === "go") {
-      const newRoom = currentRoom.getExit(target);
-      if (newRoom === null) {
+       go (target);
+      /*if (newRoom === null) {
         printResponse("You can't go that way.");
       } else {
         currentRoom = newRoom;
         describeRoom(currentRoom);
-      }
-    } else {
+      }*/
+    } else if (action == 'take' || action == 'get') {
+        take (target);
+    }
+    else if (action == 'inv' || action == 'inventory') {
+        print('You are carrying the following:<br>');
+        inventory.forEach((item) => {
+            print(item.name);
+          });
+    }
+    else {
       printResponse("I don't know how to do that.");
     }
     clearCommand();
   }
 
-  // Parse user input
-  function parseInput(input) {
-    console.log ("in parse");
-    const command = input.trim().toLowerCase();
-    const parts = command.split(' ');
-    const verb = parts[0];
-    const noun = parts.length > 1 ? parts[1] : '';
   
-    switch (verb) {
-      case "go":
-        go(noun);
-        break;
-      case "look":
-        look(noun);
-        break;
-      case "take":
-        take(noun);
-        break;
-      case "inventory":
-        showInventory();
-        break;
-      default:
-        console.log("I'm sorry, I don't understand.");
-      break;
-
-    }
-}  
 
 // Move to another room
 function go(direction) {
     const exit = rooms[currentRoom].exits[direction];
     if (exit) {
       currentRoom = exit;
-      showRoom();
+      describeRoom(currentRoom);
     } else {
       console.log("You can't go that way.");
     }
@@ -145,22 +129,45 @@ function go(direction) {
     if (noun) {
       const item = objects[noun];
       if (item) {
-        console.log(item.description);
+        print(item.description + "<br>");
       } else {
-        console.log("You don't see that here.");
+        print("You don't see that here.<br>");
       }
     } else {
       showRoom();
     }
   }
   
+  function objectIn (item, room) {
+    if (rooms[room].items.indexOf(item) > -1) return true
+    else return false;
+  }
+
+  function isObject (noun) {
+    if (objects.indexOf(noun) > -1) return true
+    else return false;
+  }
+
+  function isRoom (noun) {
+    if (rooms[noun] != undefined) return true
+    else return false;
+  }
   // Take an object
   function take(noun) {
     if (noun) {
       const item = objects[noun];
+      if (!objectIn (noun,currentRoom)) {
+        printResponse (" I don't see a " + noun + " here.");
+        return;
+      }
+
       if (item && item.canPickUp) {
         inventory.push(item);
         const index = rooms[currentRoom].items.indexOf(item.name);
+       // rooms[currentRoom].items = rooms[currentRoom].items.splice (index,1);
+       delete rooms[currentRoom].items[index];
+       print ("ok, you pick up the " + noun + "<br>");
+       describeRoom (currentRoom);
       }
     }
 }
